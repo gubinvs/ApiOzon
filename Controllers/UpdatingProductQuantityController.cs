782e296df8151dfff3492d878613eb0b74b610a8
master <gubinvs@gmail.com>
2026-09-23 15:38:43 +0300

    s

--- Controllers/UpdatingProductQuantityController.cs
+++ Controllers/UpdatingProductQuantityController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiOzon 
 {
@@ -43,40 +44,38 @@ namespace ApiOzon
             if (_password.Password == password)
             {
                 try {
-                    // Загружаем данные из таблицы товаров
-                    var product = _db.GoodsTable
-                        .ToList();
-                
-                    // Перебирая массив данных товара:
-                    foreach (var item in product)
-                    {
-                        
-                        // Находим в таблице соответствующие записи по GuidIdProduct
-                        var sku = _db.SkuOzon
-                                .Where(e => e.GuidIdProduct == item.Guid)
-                                .FirstOrDefault();
-                        
+                        // 1. Быстро собираем данные из БД за ОДИН шаг
+                        // Используем анонимный объект, чтобы вытащить только нужные для работы поля (Guid и Sku)
+                        var productsWithSku = await _db.GoodsTable
+                            .Join(
+                                _db.SkuOzon,
+                                product => product.Guid,
+                                sku => sku.GuidIdProduct,
+                                (product, sku) => new { product.Guid, sku.SkuOzon }
+                            )
+                            .ToListAsync();
 
-                        
-                        if (sku != null)
+                        if (!productsWithSku.Any()) return Ok(new {massage = "productsWithSku - пуст!"});
+
+                        // 2. Собираем все SKU в единый список для отправки в API
+                        var allSkuList = productsWithSku.Select(x => x.SkuOzon).ToList();
+
+                        // 3. Делаем ОДИН пакетный запрос к API Ozon (передаем список SKU)
+                        // Примечание: Метод GetFboQuantityBatchAsync приведен как целевой пример оптимизации API
+                        var jsonResponseOzonDate = await _ozonStockService.GetFboQuantityBatchAsync(allSkuList);
+                        var responseOzonDate = JsonSerializer.Deserialize<OzonStocksResponse>(jsonResponseOzonDate);
+
+                        // Создаем словарь для быстрого поиска остатков по SKU (O(1))
+                        var stocksBySku = responseOzonDate?.Products?
+                            .ToDictionary(p => p.Sku, p => p.Present) ?? new Dictionary<string, int>();
+
+                        // 4. Формируем итоговый список данных на обновление
+                        var warehouseDataList = productsWithSku.Select(item => new
                         {
-                            // Делаем запрос api OZON для получения данных о наличии товара на складе ОЗОН
-                            var jsonResponseOzonDate = await _ozonStockService.GetFboQuantityAsync(sku.SkuOzon);
-                            
-                            // Десериализуем в класс
-                            var responseOzonDate = JsonSerializer.Deserialize<OzonStocksResponse>(jsonResponseOzonDate);
-
-                            // Формируем данные на обновление данных по складам
-                            var warehouseDate = new
-                            {
-                                GuidIdProduct = item.Guid,
-                                Name = "Склады ОЗОН",
-                                Quantity = responseOzonDate?.Products?[0]?.Present ?? 0
-                            };
-
-    
-                        }
-                    }
+                            GuidIdProduct = item.Guid,
+                            Name = "Склады ОЗОН",
+                            Quantity = stocksBySku.TryGetValue(item.SkuOzon, out var quantity) ? quantity : 0
+                        }).ToList();
                 } 
                 catch {
                     
--- obj/Debug/net9.0/ApiOzon.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiOzon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiOzon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9edaa2913d84746e2c0c9165e6554d4f5e02753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57a037644f428a99f6845e2a90653b33ee96295")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiOzon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiOzon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
